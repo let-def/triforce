5f1b9487547b001a2c8d8b5f27be6d793d490b80
Frédéric Bour <frederic.bour@lakaban.net>
2025-05-11 08:16:56 +0900

    examples/performance_test: customize of block size

--- examples/performance_test.rs
+++ examples/performance_test.rs
@@ -1,28 +1,34 @@
 use triforce;
 
+fn usage<T, A>(_: T) -> A {
+    panic!(
+        "Usage: perf_test <duration> <blocksize>\n\
+         Simulate processing <duration> seconds of microphone input \
+         (default to 60 seconds), \
+         using blocks of <blocksize> samples (defaults to 1024)."
+    )
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let seconds: f32 = if args.len() < 2 {
         60.0
     } else {
-        match args[1].parse() {
-            Ok(x) => x,
-            Err(_) => {
-                panic!(
-                    "Usage: perf_test <duration>\n\
-                        Simulate processing <duration> seconds of microphone input \
-                        (default to 60 seconds)."
-                )
-            }
-        }
+        args[1].parse().unwrap_or_else(usage)
+    };
+    let blocksize: usize = if args.len() < 3 {
+        1024
+    } else {
+        args[2].parse().unwrap_or_else(usage)
     };
     let sample_rate = 48000.0;
     let mut inst = triforce::Triforce::with_sample_rate(48000.0);
-    let i1: Vec<f32> = (0..1024).map(|x| (x as f32) / 1024.0).collect();
-    let i2: Vec<f32> = (0..1024).map(|x| ((x as f32) + 10.0) / 1024.0).collect();
-    let i3: Vec<f32> = (0..1024).map(|x| ((x as f32) - 10.0) / 1024.0).collect();
-    let mut out: Vec<f32> = vec![0.0; 1024];
-    for _ in 0..(sample_rate * seconds / 1024.0) as i32 {
-        inst.process_slice(&i1, &i2, &i3, &mut out, 100.0, i1.len());
+    let i1: Vec<f32> = (0..blocksize).map(|x| (x as f32) / 1024.0).collect();
+    let i2: Vec<f32> = (0..blocksize).map(|x| ((x as f32) + 10.0) / 1024.0).collect();
+    let i3: Vec<f32> = (0..blocksize).map(|x| ((x as f32) - 10.0) / 1024.0).collect();
+    let mut out: Vec<f32> = Vec::new();
+    out.resize(blocksize, 0.0);
+    for _ in 0..(sample_rate * seconds / blocksize as f32) as i32 {
+        inst.process_slice(&i1, &i2, &i3, &mut out, 100.0, blocksize);
     }
 }

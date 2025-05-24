06e229f4c6d5e0dcec37075b895ef6c56d240fbc
Frédéric Bour <frederic.bour@lakaban.net>
2025-05-24 16:08:35 +0900

    Factoring IIR out

--- src/hilbert_iir.rs
+++ src/hilbert_iir.rs
@@ -1,3 +1,5 @@
+use nalgebra::Complex;
+
 const ORDER: usize = 12;
 
 #[derive(Clone, Copy)]
@@ -15,28 +17,117 @@ pub struct State {
     imag: [f32; ORDER],
 }
 
+const COEFFS: [Complex<f32>; ORDER] = [
+    Complex::<f32>::new(-0.000224352093802, 0.00543499018201),
+    Complex::<f32>::new(0.010750055781500, -0.01738906856810),
+    Complex::<f32>::new(-0.045679587391700, 0.02291669314290),
+    Complex::<f32>::new(0.112825005820000, 0.00278413661237),
+    Complex::<f32>::new(-0.208067578452000, -0.10462895867500),
+    Complex::<f32>::new(0.287178375010000, 0.33619239719000),
+    Complex::<f32>::new(-0.254675294431000, -0.68303389965500),
+    Complex::<f32>::new(0.048108183502600, 0.95406158937400),
+    Complex::<f32>::new(0.227861357867000, -0.89127357456900),
+    Complex::<f32>::new(-0.365411839137000, 0.52508831727100),
+    Complex::<f32>::new(0.280729061131000, -0.15513120660600),
+    Complex::<f32>::new(-0.093506178772800, 0.00512245855404),
+];
+
+const POLES: [Complex<f32>; ORDER] = [
+    Complex::<f32>::new(-0.00495335976478, 0.0092579876872),
+    Complex::<f32>::new(-0.01785949130200, 0.0273493725543),
+    Complex::<f32>::new(-0.04137143731550, 0.0744756910287),
+    Complex::<f32>::new(-0.08821484088850, 0.1783496774570),
+    Complex::<f32>::new(-0.17922965812000, 0.3960134022300),
+    Complex::<f32>::new(-0.33826180075300, 0.8292295333540),
+    Complex::<f32>::new(-0.55768869973200, 1.6129853832800),
+    Complex::<f32>::new(-0.73515773614800, 2.7998739868200),
+    Complex::<f32>::new(-0.71905738117200, 4.1639616612800),
+    Complex::<f32>::new(-0.51787102520900, 5.2972482680400),
+    Complex::<f32>::new(-0.28019746947100, 5.9959860238800),
+    Complex::<f32>::new(-0.08527513545310, 6.3048492377000),
+];
+
+const DIRECT: f32 = 0.000262057212648;
+
 impl Filter {
-    fn init(sample_rate: f32, passband_gain: f32) -> Coeffs {
+    pub fn init(sample_rate: f32, passband_gain: f32) -> Filter {
         let freq_factor = f32::min(0.46, 20000.0 / sample_rate);
-        let mut result = Coeffs {
+        let mut result = Filter {
             coeffs_r: [0.0; ORDER],
             coeffs_i: [0.0; ORDER],
             poles_r: [0.0; ORDER],
             poles_i: [0.0; ORDER],
             direct: 0.0,
         };
-        result.direct = CONSTANTS.direct * 2.0 * passband_gain * freq_factor;
+        result.direct = DIRECT * 2.0 * passband_gain * freq_factor;
         for i in 0..ORDER {
-            let coeff = CONSTANTS.coeffs[i] * freq_factor * passband_gain;
+            let coeff = COEFFS[i] * freq_factor * passband_gain;
             result.coeffs_r[i] = coeff.re;
             result.coeffs_i[i] = coeff.im;
-            let pole = (CONSTANTS.poles[i] * freq_factor).exp();
+            let pole = (POLES[i] * freq_factor).exp();
             result.poles_r[i] = pole.re;
             result.poles_i[i] = pole.im;
         }
         result
     }
 
-    fn process<const N: usize>(state: &mut [State; N], input: &[&[f32]; N], output: &[&mut [Complex<f32>]]) {
+    pub fn process<const N: usize>(&self, state: &mut [State; N], input: &[&[f32]; N], output: &mut [&mut [Complex<f32>]]) {
+        if N == 0 { return };
+        let samples = input[0].len();
+        for i in 0..samples {
+            let mut ra : [f32; N] = input.map(|x| x[i] * self.direct);
+            let mut ia : [f32; N] = [0f32; N];
+            for j in 0..ORDER {
+                let mut rv = [0f32; N];
+                let mut iv = [0f32; N];
+                for k in 0..N {
+                    rv[k] = state[k].real[j] * self.poles_r[j] - state[k].imag[j] * self.poles_i[j] + input[k][i] * self.coeffs_r[j];
+                    iv[k] = state[k].real[j] * self.poles_i[j] + state[k].imag[j] * self.poles_r[j] + input[k][i] * self.coeffs_i[j];
+                };
+                for k in 0..N {
+                    ra[k] += rv[k];
+                    ia[k] += iv[k];
+                    state[k].real[j] = rv[k];
+                    state[k].imag[j] = iv[k];
+                }
+            }
+            for k in 0..N {
+                output[k][i] = Complex::new(ra[k], ia[k]);
+            }
+        }
     }
+
+    pub fn process_split<const N: usize>(&self, state: &mut [State; N], input: &[&[f32]; N], real: &mut [&mut [f32]], imag: &mut [&mut [f32]]) {
+        if N == 0 { return };
+        let samples = input[0].len();
+        for i in 0..samples {
+            let mut ra : [f32; N] = input.map(|x| x[i] * self.direct);
+            let mut ia : [f32; N] = [0f32; N];
+            for j in 0..ORDER {
+                let mut rv = [0f32; N];
+                let mut iv = [0f32; N];
+                for k in 0..N {
+                    rv[k] = state[k].real[j] * self.poles_r[j] - state[k].imag[j] * self.poles_i[j] + input[k][i] * self.coeffs_r[j];
+                    iv[k] = state[k].real[j] * self.poles_i[j] + state[k].imag[j] * self.poles_r[j] + input[k][i] * self.coeffs_i[j];
+                };
+                for k in 0..N {
+                    ra[k] += rv[k];
+                    ia[k] += iv[k];
+                    state[k].real[j] = rv[k];
+                    state[k].imag[j] = iv[k];
+                }
+            }
+            for k in 0..N {
+                real[k][i] = ra[k];
+                imag[k][i] = ia[k];
+            }
+        }
+    }
+}
+
+impl State {
+    pub const INITIAL : State = State {
+        real: [-1.0; ORDER],
+        imag: [0.0; ORDER],
+    };
 }
--- src/lib.rs
+++ src/lib.rs
@@ -13,222 +13,9 @@ use std::f32::consts::PI;
 use lv2::prelude::*;
 
 use nalgebra::{linalg::SVD, Complex, Matrix3, Vector3};
+mod hilbert_iir;
 
 const C: f32 = 343.00; /* m*s^-1 */
-
-const ORDER: usize = 12;
-
-#[derive(Clone, Copy)]
-pub struct Coeffs {
-    coeffs_r: [f32; ORDER],
-    coeffs_i: [f32; ORDER],
-    poles_r: [f32; ORDER],
-    poles_i: [f32; ORDER],
-    direct: f32,
-}
-
-#[derive(Clone, Copy)]
-pub struct Filter {
-    real: [f32; ORDER],
-    imag: [f32; ORDER],
-}
-
-impl Coeffs {
-    fn init(sample_rate: f32, passband_gain: f32) -> Coeffs {
-        let freq_factor = f32::min(0.46, 20000.0 / sample_rate);
-        let mut result = Coeffs {
-            coeffs_r: [0.0; ORDER],
-            coeffs_i: [0.0; ORDER],
-            poles_r: [0.0; ORDER],
-            poles_i: [0.0; ORDER],
-            direct: 0.0,
-        };
-        result.direct = CONSTANTS.direct * 2.0 * passband_gain * freq_factor;
-        for i in 0..ORDER {
-            let coeff = CONSTANTS.coeffs[i] * freq_factor * passband_gain;
-            result.coeffs_r[i] = coeff.re;
-            result.coeffs_i[i] = coeff.im;
-            let pole = (CONSTANTS.poles[i] * freq_factor).exp();
-            result.poles_r[i] = pole.re;
-            result.poles_i[i] = pole.im;
-        }
-        result
-    }
-}
-
-impl Filter {
-    const INITIAL : Filter = Filter {
-        real: [-1.0; ORDER],
-        imag: [0.0; ORDER],
-    };
-
-    // fn sample(&mut self, coeffs: &Coeffs, x: f32) -> (f32, f32) {
-    //     let mut result_r = x * coeffs.direct;
-    //     let mut result_i = 0.0;
-    //     for i in 0..ORDER {
-    //         let real = self.real[i] * coeffs.poles_r[i] - self.imag[i] * coeffs.poles_i[i] + x * coeffs.coeffs_r[i];
-    //         let imag = self.real[i] * coeffs.poles_i[i] + self.imag[i] * coeffs.poles_r[i] + x * coeffs.coeffs_i[i];
-    //         result_r += real;
-    //         self.real[i] = real;
-    //         result_i += imag;
-    //         self.imag[i] = imag;
-    //     }
-    //     (result_r, result_i)
-    // }
-
-    // fn process(&mut self, coeffs: &Coeffs, x: &[f32], r: &mut [f32], i: &mut [f32]) {
-    //     for (x, r, i) in izip!(x, r, i) {
-    //         let (rr, ri) = self.sample(coeffs, *x);
-    //         *r = rr;
-    //         *i = ri;
-    //     }
-    // }
-
-    // fn process2(
-    //     coeffs: &Coeffs,
-    //     state: &mut [Self; 2],
-    //     x0: &[f32],
-    //     x1: &[f32],
-    //     r0: &mut [f32],
-    //     r1: &mut [f32],
-    //     i0: &mut [f32],
-    //     i1: &mut [f32],
-    // ) {
-    //     for (x0, x1, r0, r1, i0, i1) in izip!(x0, x1, r0, r1, i0, i1) {
-    //         let mut r0v = x0 * coeffs.direct;
-    //         let mut i0v = 0.0;
-    //         let mut r1v = x1 * coeffs.direct;
-    //         let mut i1v = 0.0;
-    //         for j in 0..ORDER {
-    //             let real0 = state[0].real[j] * coeffs.poles_r[j]
-    //                 - state[0].imag[j] * coeffs.poles_i[j]
-    //                 + x0 * coeffs.coeffs_r[j];
-    //             let imag0 = state[0].real[j] * coeffs.poles_i[j]
-    //                 + state[0].imag[j] * coeffs.poles_r[j]
-    //                 + x0 * coeffs.coeffs_i[j];
-    //             let real1 = state[1].real[j] * coeffs.poles_r[j]
-    //                 - state[1].imag[j] * coeffs.poles_i[j]
-    //                 + x1 * coeffs.coeffs_r[j];
-    //             let imag1 = state[1].real[j] * coeffs.poles_i[j]
-    //                 + state[1].imag[j] * coeffs.poles_r[j]
-    //                 + x1 * coeffs.coeffs_i[j];
-    //             r0v += real0;
-    //             state[0].real[j] = real0;
-    //             i0v += imag0;
-    //             state[0].imag[j] = imag0;
-    //             r1v += real1;
-    //             state[1].real[j] = real1;
-    //             i1v += imag1;
-    //             state[1].imag[j] = imag1;
-    //         }
-    //         *r0 = r0v;
-    //         *i0 = i0v;
-    //         *r1 = r1v;
-    //         *i1 = i1v;
-    //     }
-    // }
-
-    fn process3(
-        coeffs: &Coeffs,
-        state: &mut [Self; 3],
-        x0: &[f32],
-        x1: &[f32],
-        x2: &[f32],
-        r0: &mut [f32],
-        r1: &mut [f32],
-        r2: &mut [f32],
-        i0: &mut [f32],
-        i1: &mut [f32],
-        i2: &mut [f32],
-    ) {
-        let samples = x0.len();
-        for i in 0..samples {
-            let mut r0_val = x0[i] * coeffs.direct;
-            let mut i0_val = 0.0;
-            let mut r1_val = x1[i] * coeffs.direct;
-            let mut i1_val = 0.0;
-            let mut r2_val = x2[i] * coeffs.direct;
-            let mut i2_val = 0.0;
-            for j in 0..ORDER {
-                let real0 = state[0].real[j] * coeffs.poles_r[j]
-                    - state[0].imag[j] * coeffs.poles_i[j]
-                    + x0[i] * coeffs.coeffs_r[j];
-                let imag0 = state[0].real[j] * coeffs.poles_i[j]
-                    + state[0].imag[j] * coeffs.poles_r[j]
-                    + x0[i] * coeffs.coeffs_i[j];
-                let real1 = state[1].real[j] * coeffs.poles_r[j]
-                    - state[1].imag[j] * coeffs.poles_i[j]
-                    + x1[i] * coeffs.coeffs_r[j];
-                let imag1 = state[1].real[j] * coeffs.poles_i[j]
-                    + state[1].imag[j] * coeffs.poles_r[j]
-                    + x1[i] * coeffs.coeffs_i[j];
-                let real2 = state[2].real[j] * coeffs.poles_r[j]
-                    - state[2].imag[j] * coeffs.poles_i[j]
-                    + x2[i] * coeffs.coeffs_r[j];
-                let imag2 = state[2].real[j] * coeffs.poles_i[j]
-                    + state[2].imag[j] * coeffs.poles_r[j]
-                    + x2[i] * coeffs.coeffs_i[j];
-                r0_val += real0;
-                state[0].real[j] = real0;
-                i0_val += imag0;
-                state[0].imag[j] = imag0;
-                r1_val += real1;
-                state[1].real[j] = real1;
-                i1_val += imag1;
-                state[1].imag[j] = imag1;
-                r2_val += real2;
-                state[2].real[j] = real2;
-                i2_val += imag2;
-                state[2].imag[j] = imag2;
-            }
-            r0[i] = r0_val;
-            i0[i] = i0_val;
-            r1[i] = r1_val;
-            i1[i] = i1_val;
-            r2[i] = r2_val;
-            i2[i] = i2_val;
-        }
-    }
-
-}
-
-struct Constants {
-    coeffs: [Complex<f32>; ORDER],
-    poles: [Complex<f32>; ORDER],
-    direct: f32,
-}
-
-const CONSTANTS: Constants = Constants {
-    coeffs: [
-        Complex::<f32>::new(-0.000224352093802, 0.00543499018201),
-        Complex::<f32>::new(0.010750055781500, -0.01738906856810),
-        Complex::<f32>::new(-0.045679587391700, 0.02291669314290),
-        Complex::<f32>::new(0.112825005820000, 0.00278413661237),
-        Complex::<f32>::new(-0.208067578452000, -0.10462895867500),
-        Complex::<f32>::new(0.287178375010000, 0.33619239719000),
-        Complex::<f32>::new(-0.254675294431000, -0.68303389965500),
-        Complex::<f32>::new(0.048108183502600, 0.95406158937400),
-        Complex::<f32>::new(0.227861357867000, -0.89127357456900),
-        Complex::<f32>::new(-0.365411839137000, 0.52508831727100),
-        Complex::<f32>::new(0.280729061131000, -0.15513120660600),
-        Complex::<f32>::new(-0.093506178772800, 0.00512245855404),
-    ],
-    poles: [
-        Complex::<f32>::new(-0.00495335976478, 0.0092579876872),
-        Complex::<f32>::new(-0.01785949130200, 0.0273493725543),
-        Complex::<f32>::new(-0.04137143731550, 0.0744756910287),
-        Complex::<f32>::new(-0.08821484088850, 0.1783496774570),
-        Complex::<f32>::new(-0.17922965812000, 0.3960134022300),
-        Complex::<f32>::new(-0.33826180075300, 0.8292295333540),
-        Complex::<f32>::new(-0.55768869973200, 1.6129853832800),
-        Complex::<f32>::new(-0.73515773614800, 2.7998739868200),
-        Complex::<f32>::new(-0.71905738117200, 4.1639616612800),
-        Complex::<f32>::new(-0.51787102520900, 5.2972482680400),
-        Complex::<f32>::new(-0.28019746947100, 5.9959860238800),
-        Complex::<f32>::new(-0.08527513545310, 6.3048492377000),
-    ],
-    direct: 0.000262057212648,
-};
 
 /// The distance of a given element in the array from the zeroth
 /// element
@@ -354,8 +141,8 @@ pub struct Triforce {
     covar: Matrix3<Complex<f32>>,
     array_geom: [ElemDistance; 3],
     weights: Vector3<Complex<f32>>,
-    analytic_coeffs: Coeffs,
-    analytic_filters: [Filter; 3],
+    analytic_filter: hilbert_iir::Filter,
+    analytic_state: [hilbert_iir::State; 3],
     input_real0: Vec<f32>,
     input_real1: Vec<f32>,
     input_real2: Vec<f32>,
@@ -387,8 +174,8 @@ impl Triforce {
             ),
             covar: Matrix3::zeros(),
             weights: Vector3::zeros(),
-            analytic_coeffs: Coeffs::init(sample_rate, 2.0),
-            analytic_filters: [Filter::INITIAL,Filter::INITIAL,Filter::INITIAL],
+            analytic_filter: hilbert_iir::Filter::init(sample_rate, 2.0),
+            analytic_state: [hilbert_iir::State::INITIAL; 3],
             input_real0: Vec::new(),
             input_real1: Vec::new(),
             input_real2: Vec::new(),
@@ -414,14 +201,10 @@ impl Triforce {
         self.input_imag1.resize(buf_len, 0.0);
         self.input_real2.resize(buf_len, 0.0);
         self.input_imag2.resize(buf_len, 0.0);
-        Filter::process3(&self.analytic_coeffs, &mut self.analytic_filters,
-                         mic1, mic2, mic3,
-                         &mut self.input_real0,
-                         &mut self.input_real1,
-                         &mut self.input_real2,
-                         &mut self.input_imag0,
-                         &mut self.input_imag1,
-                         &mut self.input_imag2);
+        self.analytic_filter.process_split(&mut self.analytic_state,
+                                           &[mic1, mic2, mic3],
+                                           &mut [&mut self.input_real0, &mut self.input_real1, &mut self.input_real2],
+                                           &mut [&mut self.input_imag0, &mut self.input_imag1, &mut self.input_imag2]);
 
         // Update the covariance matrix. We use an overlapping window to smooth over
         // the transitions.
@@ -480,7 +263,7 @@ impl Plugin for Triforce {
         Some(Self::with_sample_rate(info.sample_rate() as f32))
     }
 
-    fn run(&mut self, ports: &mut Ports, _features: &mut (), _: u32) {
+    fn run(&mut self, ports: &mut Ports, _features: &mut (), samples: u32) {
         Beamformer::update_params(self, ports);
         self.process_slice(
             &ports.in_1,
